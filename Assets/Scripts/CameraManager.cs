fb1be5df16c13e587923da9a8639d48e5e16ca2c
SpeedAmigo <Kxipi7@gmail.com>
2024-03-18 19:08:03 +0100

    canon

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -29,12 +29,6 @@ public class CameraManager : MonoBehaviour
         camRotation.x += Input.GetAxis("Mouse X") * sensivity;
         camRotation.y += Input.GetAxis("Mouse Y") * sensivity;
 
-        //float mouseX = Input.GetAxis("Mouse X") * sensivity;
-        //float mouseY = Input.GetAxis("Mouse Y") * sensivity;
-
-        //Debug.Log(mouseY);
-        //Debug.Log(mouseX);
-
         gObject.transform.rotation = Quaternion.Euler(camRotation.y, camRotation.x, 0);
 
         camRotation.y = Mathf.Clamp(camRotation.y, lookMin, lookMax);
